7dd7f31065c53de89ab8a1f1d0be60cfcfbcf12c
larissaleite <larissa.leite@gmail.com>
2016-10-19 03:46:29 +0200

    Added ingredients and recipe characteristics

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,11 @@ from app.models import *
 from datetime import datetime
 
 import requests
+import logging
+
+#check error Broken Pipe with requests lib
+logging.getLogger('requests').setLevel(logging.CRITICAL)
+
 
 headers = {
 	"X-Mashape-Key": "62t27Bg1q7mshtmPnpbNVMOAVw9Tp1PkKrqjsnebv8Ph00q2x3",
@@ -24,44 +29,49 @@ def profile():
 	if request.method == 'GET':
 		return render_template('profile.html', user=current_user)
 	else:
-		#user...
 		return home()
 
 @app.route('/register', methods=['POST'])
 def register():
-    print request
-    age = request.form['age']
-    gender = request.form['gender']
-    location = request.form['location']
+	age = request.form['age']
+	gender = request.form['gender']
+	location = request.form['location']
 
-    print "------ "+age+" " + " " + location
+	user = User.objects.filter(id=current_user.id).first()
 
-    user = User.objects.filter(id=current_user.id).first()
+	user.update(**{
+		'set__age' : age,
+		'set__gender': gender,
+		'set__location' : location
+	})
 
-    '''user.age = age
-    user.gender = gender
-    user.location = location
-
-    user.save()'''
-
-    user.update(**{
-        'set__age' : age,
-        'set__gender': gender,
-        'set__location' : location
-    })
-
-    return home()
+	return home()
 
 
 @app.route('/home', methods=['GET'])
 @login_required
 def home():
-	response = requests.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1", headers=headers)
+	response = requests.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=5", headers=headers)
 	data = response.json()
 
 	recipes = []
 
 	for recipe in data['recipes']:
-		recipes.append({ 'title' : recipe['title'], 'img' : recipe['image'], 'instructions' : recipe['instructions'] })
+		ingredients = []
+
+		for ingredient in recipe['extendedIngredients']:
+			ingredients.append({
+				'text' : str(ingredient['amount']) + " " + ingredient['unit'] + " of " + ingredient['name']
+			})
+
+		recipes.append({
+			'title' : recipe['title'],
+			'img' : recipe['image'],
+			'instructions' : recipe['instructions'],
+			'vegetarian' : recipe['vegetarian'],
+			'glutenFree' : recipe['glutenFree'],
+			'dairyFree' : recipe['dairyFree'],
+			'ingredients' : ingredients
+		})
 
 	return render_template("home.html", recipes=recipes)
