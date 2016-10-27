d18be207101baf488f178d791c35711f1bb0c1d6
larissaleite <larissa.leite@gmail.com>
2016-10-28 00:49:08 +0200

    Minor updates

--- data-collector.py
+++ data-collector.py
@@ -9,38 +9,47 @@ for filename in glob.glob(os.getcwd()+'/data/*.json'):
 
 			recipe_data = recipe_data['recipe']
 
-			summary = ""
-			if 'summary' in recipe_data:
-				summary = recipe_data['summary']
+			title = recipe_data['label']
 
-			recipe = Recipe(
-				title=recipe_data['label'],
-				image=recipe_data['image'],
-				calories=recipe_data['calories'],
-				instructions=summary
-			)
+			if Recipe.objects.filter(title=title).first() is None:
 
-			for label in recipe_data['healthLabels']:
-				if label == 'vegan':
-					recipe.vegan = True
-				elif label == 'vegetarian':
-					recipe.vegetarian = True
-				elif label == 'gluten-free':
-					recipe.glutenFree = True
-				elif label == 'dairy-free':
-					recipe.dairyFree = True
-				elif label == 'peanut-free':
-					recipe.peanutFree = True
-				elif label == 'fat-free':
-					recipe.fatFree = True
+				summary = ""
+				if 'summary' in recipe_data:
+					summary = recipe_data['summary']
 
-			for ingredient in recipe_data['ingredients']:
-				ingredient = ExtendedIngredient(
-					name=ingredient['food'],
-					amount=ingredient['quantity'],
-					unit=ingredient['measure'],
-					full_text=ingredient['text']
+				recipe = Recipe(
+					title=title,
+					image=recipe_data['image'],
+					calories=recipe_data['calories'],
+					instructions=summary
 				)
-				recipe.ingredients.append(ingredient)
 
-			recipe.save()
+				for label in recipe_data['healthLabels']:
+					if label == 'Vegan':
+						recipe.vegan = True
+					elif label == 'Vegetarian':
+						recipe.vegetarian = True
+					elif label == 'Gluten-Free':
+						recipe.glutenFree = True
+					elif label == 'Dairy-Free':
+						recipe.dairyFree = True
+					elif label == 'Peanut-Free':
+						recipe.peanutFree = True
+					elif label == 'Fat-Free':
+						recipe.fatFree = True
+
+				for ingredient in recipe_data['ingredients']:
+					full_text=ingredient['text']
+
+					ingredient = ExtendedIngredient(
+						name=ingredient['food'],
+						amount=ingredient['quantity'],
+						unit=ingredient['measure'],
+					)
+
+					if full_text is not None and len(full_text) < 490:
+						ingredient.full_text = full_text
+
+					recipe.ingredients.append(ingredient)
+
+				recipe.save()
