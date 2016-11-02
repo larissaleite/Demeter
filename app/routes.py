593056fd86673452e9ee43ad7928abce234584e5
larissaleite <larissa.leite@gmail.com>
2016-11-02 01:57:58 +0100

    Profile page working except default location

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,7 @@ def index():
 	return render_template('index.html')
 
 @app.route('/profile', methods=['GET', 'POST'])
+@login_required
 def profile():
 	if request.method == 'GET':
 		return render_template('profile.html', user=current_user)
@@ -21,16 +22,35 @@ def profile():
 
 @app.route('/register', methods=['POST'])
 def register():
-	age = request.form['age']
-	gender = request.form['gender']
-	location = request.form['location']
+	age = request.json['age']
+	gender = request.json['gender']
+	location = request.json['location']
+	restrictions = request.json['restrictions']
+	ingredients = request.json['ingredients']
+
+	preferred_ingredients = []
+	ingredient_restrictions = []
+
+	for ingredient_name in ingredients:
+		ingredient = Ingredient(
+			name = ingredient_name
+		)
+		preferred_ingredients.append(ingredient)
+
+	for ingredient_name in restrictions:
+		ingredient = Ingredient(
+			name = ingredient_name
+		)
+		ingredient_restrictions.append(ingredient)
 
 	user = User.objects.filter(id=current_user.id).first()
 
 	user.update(**{
 		'set__age' : age,
 		'set__gender': gender,
-		'set__location' : location
+		'set__location' : location,
+		'set__preferred_ingredients':preferred_ingredients,
+		'set__allergies':ingredient_restrictions
 	})
 
 	return home()
@@ -66,3 +86,27 @@ def home():
 		})
 
 	return render_template("home.html", recipes=recipes)
+
+@app.route('/api/user', methods=['GET'])
+def get_user():
+	user = User.objects.filter(id=current_user.id).first()
+
+	user = {
+		'age' : user.age,
+		'gender' : user.gender,
+		'location' : user.location,
+		'ingredients' : user.preferred_ingredients,
+		'restrictions' : user.allergies
+	};
+
+	return jsonify(user=user)
+
+@app.route('/api/ingredients', methods=['GET'])
+def get_ingredients():
+	all_ingredients = Recipe.objects.distinct(field="ingredients.name")
+
+	return jsonify( { 'all_ingredients': all_ingredients } )
+
+@app.route('/template_select', methods = ['GET'])
+def get_template_select():
+	return render_template('template_select.html')
